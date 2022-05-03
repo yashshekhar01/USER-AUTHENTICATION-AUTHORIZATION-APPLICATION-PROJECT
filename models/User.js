2e148cfc02d4d1005c9845c2ed81a43c1cb1db3f
yashshekhar01 <yashshekhar2934@gmail.com>
2022-05-03 16:12:47 +0530

    Auth Controller Logic Part I

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -23,6 +24,20 @@ const UserSchema = new mongoose.Schema({
     resetPasswordToken: String,
     resetPasswordExpire: Date,
 });
+
+UserSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.matchPasswords = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", UserSchema);
 
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const connectDB = require('./config/db');
+
+// Connect DB
+connectDB();
 
 const app = express();
 
@@ -9,4 +13,11 @@ app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+console.log(`Server running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Logged Error: ${err}`);
+    server.close(() => process.exit(1)); 
+});
