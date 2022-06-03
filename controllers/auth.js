6385d1c0b99f671c875909afb93848ac4cecd5fb
yashshekhar01 <yashshekhar2934@gmail.com>
2022-06-04 02:51:46 +0530

    Custom Error Handling Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -15,10 +16,7 @@ exports.register = async (req, res, next) => {
             user,
         });
     } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        });
+       next(error);
     }
 };
 
@@ -26,22 +24,20 @@ exports.login = async (req, res, next) => {
     const { email, password } = req.body;
 
     if(!email || !password) {
-        res 
-        .status(400)
-        .json({ success: false, error: "Please provide email and password"});
+        return next(new ErrorResponse("Please provide an email and password", 400))
     }
 
     try {
         const user = await User.findOne({ email }).select("+password");
 
         if(!user) {
-            res.status(404).json({ success: false, error: "Invalid credentials"})
+            return next(new ErrorResponse("Please provide an email and password", 401))
         }
 
         const isMatch = await user.matchPasswords(password);
 
         if(!isMatch) {
-            res.status(404).json({success: false, error: "Invalid credentials"})
+            return next(new ErrorResponse("Please provide an email and password", 401))
         }
 
         res.status(200).json({
