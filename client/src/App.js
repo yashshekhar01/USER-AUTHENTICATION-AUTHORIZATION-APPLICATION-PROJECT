b41fedf1fae86c7a0623196aea822899841d17fe
yashshekhar01 <yashshekhar2934@gmail.com>
2022-06-08 00:32:57 +0530

    setting up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <Router>
+  <div className="app">
+    <Switch>
+      <Route exact path="/login" component={LoginScreen} />
+      <Route exact path="/register" component={RegisterScreen} />
+
+      <Route
+       exact 
+       path="/forgotpassword"
+       component={ForgotPasswordScreen} 
+       />
+      <Route 
+       exact path="/passwordreset/:resetToken"
+       component={ResetPasswordScreen} 
+       />
+      </Switch>
+      </div>
+      </Router>)
 };  
 
 export default App;
+
